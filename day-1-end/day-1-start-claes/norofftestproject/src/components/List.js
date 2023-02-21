bac94780ed007ae647620c59f731ba5cd6da7573
ClaesF0 <claes_folkestad@hotmail.com>
2023-02-21 14:36:09 +0100

    first day react

---
+++ day-1-end/day-1-start-claes/norofftestproject/src/components/List.js
@@ -0,0 +1,11 @@
+function List() {
+  return (
+    <ul>
+      <li>Amerikansk fotball😀</li>
+      <li>Reise😀</li>
+      <li>Historie😀</li>
+    </ul>
+  );
+}
+
+export default List;
---
+++ day-1-end/day-1-start-claes/norofftestproject/src/components/Paragraph.js
@@ -0,0 +1,12 @@
+function Paragraph() {
+  return (
+    <p>
+      hellorem ipsum As you can see, the prop Text is capitalized, which is a
+      requirement in React. So in summary, capitalization in React is not just a
+      best practice, but it is necessary for certain elements and props to be
+      properly recognized and rendered.
+    </p>
+  );
+}
+
+export default Paragraph;
