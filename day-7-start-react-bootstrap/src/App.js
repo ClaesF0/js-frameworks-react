af524f03de08b2f0e7dee6c0bd01f82f62a940f9
ClaesF0 <claes_folkestad@hotmail.com>
2023-03-03 12:12:49 +0100

    day-7-bootstrap-react

--- day-7-start-react-bootstrap/src/App.js
+++ day-7-start-react-bootstrap/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import "./App.css";
+import Nav from "react-bootstrap/Nav";
+import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
     </div>
   );
 }
