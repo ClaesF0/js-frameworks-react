3a52328f78037078df947e70ff27c77746dae54f
ClaesF0 <claes_folkestad@hotmail.com>
2023-03-09 19:04:31 +0100

    Day 11 done

---
+++ day-11-start--use_reducer/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/BasicExample";
+import BasicExample from "./components/BasicExample";
+
+function App() {
+  return (
+    <div className="App">
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
